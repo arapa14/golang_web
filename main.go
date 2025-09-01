42caadc051477bc2fd0262546f2833f4361dcbdc
arapa14 <arapa00016@gmail.com>
2025-09-01 20:54:08 +0700

    Hello world web

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", helloHandler)
+
+	log.Println("Starting web on port 8080")
+
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world, im learning Golank"))
+}
