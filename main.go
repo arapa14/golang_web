4f326a38fa8f1b11be04b892a5b67b55ab0cc1ee
arapa14 <arapa00016@gmail.com>
2025-09-01 21:20:18 +0700

    routing request

--- main.go
+++ main.go
@@ -8,14 +8,28 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", helloHandler)
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/Hello", helloHandler)
+	mux.HandleFunc("/mario", marioHandler)
 
-	log.Println("Starting web on port 8080")
+	log.Println("Starting on port 8080")
 
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Ini landing page"))
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world, im learning Golank"))
+	w.Write([]byte("Hello dunia, aku lagi belajar golank"))
+}
+
+func marioHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Mario from nitendo"))
 }
